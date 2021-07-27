49f1c775760d82e90947a3fe2d38794e5cf2eeb3
Irene <irene.rgarcia13@gmail.com>
2021-07-27 21:12:17 +0200

    Navigating menu fxed

--- arBUA/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
+++ arBUA/Assets/GoogleARCore/Examples/AugmentedImage/Scripts/AugmentedImageVisualizer.cs
@@ -28,6 +28,7 @@ namespace GoogleARCore.Examples.AugmentedImage
     using UnityEngine;
     using UnityEngine.UI;
     using System.Collections;
+    using UnityEngine.SceneManagement;
 
     /// <summary>
     /// Uses 4 frame corner objects to visualize an AugmentedImage.
@@ -38,6 +39,8 @@ namespace GoogleARCore.Examples.AugmentedImage
         /// The AugmentedImage to visualize.
         /// </summary>
         public AugmentedImage Image;
+
+        public GameObject RA;
 
         /// <summary>
         /// A model for the lower left corner of the frame to place when an image is detected.
@@ -61,47 +64,19 @@ namespace GoogleARCore.Examples.AugmentedImage
 
         public GameObject final;
 
+        public Canvas screen1;
+        public Canvas screen2;
+
         /// Mio
+        /// 
+
+        public RawImage rawImage;
 
         public Button b1_policien;
-        public Button b2_filoletras;
-
-        public Button b11_poli;
-        public Button b12_cien;
-
-
-        public Button ingen;
-        public Button arte;
-        public Button cons;
-        public Button arqu;
-        public Button info;
-        public Button urb;
-        public Button ingq;
-        public Button ast;
-        public Button pin;
-
-        //public Button[] b111_poli = {ingen, arte, cons, arqu, info, ingq, ast, pin, urb };
+     
         private int activo1 = 1;
         private int activo2 = 0;
         private int activo3 = 0;
-
-
-        public ArrayList b111_poli = new ArrayList();
-
-        public AugmentedImageVisualizer()
-        {
-            //Button[]  = { ingen, arte, cons, arqu, info, ingq, ast, pin, urb };
-            b111_poli.Add(ingen);
-            b111_poli.Add(arte);
-            b111_poli.Add(cons);
-            b111_poli.Add(arqu);
-            b111_poli.Add(info);
-            b111_poli.Add(ingq);
-            b111_poli.Add(ast);
-            b111_poli.Add(pin);
-            b111_poli.Add(urb);
-
-        }
 
         public void activar1()
         {
@@ -114,91 +89,48 @@ namespace GoogleARCore.Examples.AugmentedImage
         {
             activo1 = 0;
             activo2 = 0;
-            activo3 = 1;
-        }
-
-        public void activar3()
-        {
-            activo1 = 0;
-            activo2 = 0;
             activo3 = 0;
         }
+
+
 
         /// <summary>
         /// The Unity Update method.
         /// </summary>
         public void Update()
         {
-            if (Image == null || Image.TrackingState != TrackingState.Tracking || activo1==1 || activo2 == 1 || activo3 == 1)
+
+            //Si no se ha escaneado la imagen todavia o no se ha activado la realidad aumentada
+            //deben permanecer ocultos los objetos 3d del camino
+            if (Image == null || Image.TrackingState != TrackingState.Tracking || activo1==1 || activo2==1)
             {
-                if (activo2 == 1)
-                {
-
-                    b1_policien.gameObject.SetActive(false);
-                    b2_filoletras.gameObject.SetActive(false);
-                    b11_poli.gameObject.SetActive(true);
-                    b12_cien.gameObject.SetActive(true);
-
-                    foreach (Button bt in b111_poli)
-                    {
-                        bt.gameObject.SetActive(false);
-                    }
-                }
-                else if (activo3 == 1)
-                {
-                    b1_policien.gameObject.SetActive(false);
-                    b2_filoletras.gameObject.SetActive(false);
-                    b11_poli.gameObject.SetActive(false);
-                    b12_cien.gameObject.SetActive(false);
-                    foreach (Button bt in b111_poli)
-                    {
-                        bt.gameObject.SetActive(true);
-                    }
-                } else if (activo1 == 1)
+                if (activo1 == 1)
                 {
                     b1_policien.gameObject.SetActive(true);
-                    b2_filoletras.gameObject.SetActive(true);
-                    b11_poli.gameObject.SetActive(false);
-                    b12_cien.gameObject.SetActive(false);
-
-                    foreach (Button bt in b111_poli)
-                    {
-                        bt.gameObject.SetActive(false);
-                    }
+                    rawImage.gameObject.SetActive(true);
+                    screen2.gameObject.SetActive(false);
+                } else if (activo2 == 1)
+                {
+                    screen1.gameObject.SetActive(false);
+                    screen2.gameObject.SetActive(true);
                 }
-                
 
-                arrow1.SetActive(false);
-                arrow2.SetActive(false);
-                arrow3.SetActive(false);
-                arrow4.SetActive(false);
-                final.SetActive(false);
+                RA.SetActive(false);
+
                 return;
-            }
-
-            
-
-            arrow1.transform.position = new Vector3(0f, 0f, 0.500f);
-            arrow2.transform.position = new Vector3(0f, 0.1f, 1f);
-            arrow3.transform.position = new Vector3(0f, 0.2f, 2f);
-            arrow4.transform.position = new Vector3(0f, 0.3f, 3f);
-            final.transform.position = new Vector3(0f, 0.4f, 4f);
+            } 
+                arrow1.transform.position = new Vector3(0f, 0f, 0.500f);
+                arrow2.transform.position = new Vector3(0f, 0.1f, 1f);
+                arrow3.transform.position = new Vector3(0f, 0.2f, 2f);
+                arrow4.transform.position = new Vector3(0f, 0.3f, 3f);
+                final.transform.position = new Vector3(0f, 0.4f, 4f);
 
 
-            b1_policien.gameObject.SetActive(false);
-            b2_filoletras.gameObject.SetActive(false);
-            b11_poli.gameObject.SetActive(false);
-            b12_cien.gameObject.SetActive(false);
-            foreach (Button bt in b111_poli)
-            {
-                bt.gameObject.SetActive(false);
-            }
+                RA.SetActive(true);
+                screen1.gameObject.SetActive(false);
+                screen2.gameObject.SetActive(false);
+           
 
-            arrow1.SetActive(true);
-            arrow2.SetActive(true);
-            arrow3.SetActive(true);
-            arrow4.SetActive(true);
-            final.SetActive(true);
         }
     }
 }
